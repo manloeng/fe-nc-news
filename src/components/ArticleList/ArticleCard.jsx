8e27278193cceda463a4dc59d3b045e65a3444fc
Andrew <manloengchung@yahoo.co.uk>
2019-08-05 16:15:46 +0100

    remove uneeded code

--- src/components/ArticleList/ArticleCard.jsx
+++ src/components/ArticleList/ArticleCard.jsx
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import './ArticleCard.css';
 import { Link } from '@reach/router';
-import Article from './Article';
 
 class ArticleCard extends Component {
-	state = {
-		fetchArticleById: null
-	};
-
 	render() {
 		const { article: { author, comment_count, created_at, title, topic, votes, article_id } } = this.props;
 		return (
 			<ul id="articleCard">
 				<li>
-					<Link to={`/${article_id}`} id="titleButton">
-						{title}
-					</Link>
+					<Link to={`/${article_id}`}>{title}</Link>
 					<p>Comment Count: {comment_count}</p>
 					<p>Topic: {topic}</p>
 					<p>By: {author}</p>
@@ -25,15 +18,6 @@ class ArticleCard extends Component {
 			</ul>
 		);
 	}
-
-	handleClick = (e) => {
-		this.setState({ fetchArticleById: e.target.name });
-	};
-
-	handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(e);
-	};
 }
 
 export default ArticleCard;
