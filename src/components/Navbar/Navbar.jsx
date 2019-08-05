71165dd533f621cfcbb5644287677707fbd928f2
Andrew <manloengchung@yahoo.co.uk>
2019-08-05 17:08:40 +0100

    add navigation links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import './Navbar.css';
+import { Link } from '@reach/router';
 
 const Navbar = ({ topicsData: { topics } }) => {
 	return (
 		<nav id="navbar">
-			<ul>
-				<Row>
-					<li>Home</li>
-				</Row>
-				{topics.map((topic) => {
-					const formattedSlug = topic.slug[0].toUpperCase() + topic.slug.slice(1);
-					return (
-						<Row key={topic.slug}>
-							<li>{formattedSlug}</li>
-						</Row>
-					);
-				})}
-			</ul>
+			<Row id="profile" />
+			<Row>
+				<Link to="/">Home</Link>
+			</Row>
+			{topics.map((topic) => {
+				const formattedSlug = topic.slug[0].toUpperCase() + topic.slug.slice(1);
+				return (
+					<Row key={topic.slug}>
+						<Link to={`/${topic.slug}`}>{formattedSlug}</Link>
+					</Row>
+				);
+			})}
 		</nav>
 	);
 };
