d7ae7495db9c94a0feecaaeb2575fe309282c8dc
Andrew <manloengchung@yahoo.co.uk>
2019-08-05 21:31:15 +0100

    add key to topic list

--- src/components/TopicList/TopicList.jsx
+++ src/components/TopicList/TopicList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const TopicList = ({ topicsData }) => {
-	console.log(topicsData);
 	return (
-		<div>
-			{topicsData.topics.map((topic) => {
-				return <h2>{topic.slug}</h2>;
-			})}
-		</div>
+		<section>
+			<ul>
+				{topicsData.topics.map((topic) => {
+					return (
+						<li key={topic.slug}>
+							<h2>{topic.slug}</h2>
+						</li>
+					);
+				})}
+			</ul>
+		</section>
 	);
 };
 
