9f76e4a94c86333e6a555397b0792ab654269529
Andrew <manloengchung@yahoo.co.uk>
2019-08-05 14:24:55 +0100

    create articleList component with basic styling

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './ArticleList.css';
+import ArticleCard from './ArticleCard';
 
 class ArticleList extends Component {
+	state = {
+		articleListData: null
+	};
+
+	componentDidMount() {
+		axios.get('https://project-nc-news.herokuapp.com/api/articles').then(({ data }) => {
+			this.setState({ articleListData: data });
+		});
+	}
+
 	render() {
+		const { articleListData } = this.state;
 		return (
-			<section id="articleList">
-				<article>
-					<p>Article List here!</p>
-				</article>
-			</section>
+			articleListData && (
+				<section id="articleCardSection">
+					<article>
+						{articleListData.articles.map((article) => {
+							return <ArticleCard article={article} key={article.article_id} />;
+						})}
+					</article>
+				</section>
+			)
 		);
 	}
 }
