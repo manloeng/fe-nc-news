252f08816502cb75e3019bf78a46a302aad912bf
Andrew <manloengchung@yahoo.co.uk>
2019-08-06 16:23:53 +0100

    add 3rd test for article input

--- src/test/formatArticleInput.js
+++ src/test/formatArticleInput.js
@@ -1,5 +1,14 @@
-const formatArticleInput = () => {
-	return {};
+const formatArticleInput = (input) => {
+	if (!Object.keys(input).length) return {};
+	const { articleTitle, topicInput, ...restOfTheArticleBody } = input;
+	if (articleTitle) {
+		restOfTheArticleBody.title = articleTitle;
+	}
+	if (topicInput) {
+		restOfTheArticleBody.topic = topicInput;
+	}
+
+	return restOfTheArticleBody;
 };
 
 export default formatArticleInput;
