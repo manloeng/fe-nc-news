2aacf11026f78ff8467d1d6a5814780e9892e83a
Andrew <manloengchung@yahoo.co.uk>
2019-08-06 11:37:47 +0100

    add loading to user page

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './user.css';
 
 const User = ({ userData }) => {
-	return (
-		userData && (
-			<figure id="userProfile">
-				<img src={userData.user.avatar_url} alt="users avatar" />
-				<p>Username: {userData.user.username}</p>
-				<p>Name: {userData.user.name}</p>
-			</figure>
-		)
+	return !userData ? (
+		<p>loading...</p>
+	) : (
+		<figure id="userProfile">
+			<img src={userData.user.avatar_url} alt="users avatar" />
+			<p>Username: {userData.user.username}</p>
+			<p>Name: {userData.user.name}</p>
+		</figure>
 	);
 };
 
