c97d91a146f5fab1d3900c3ad2a5459e668ca799
Andrew <manloengchung@yahoo.co.uk>
2019-08-06 11:51:32 +0100

    refactor to store user data

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as api from '../api';
 import './user.css';
 
-const User = ({ userData }) => {
-	return !userData ? (
-		<p>loading...</p>
-	) : (
-		<figure id="userProfile">
-			<img src={userData.user.avatar_url} alt="users avatar" />
-			<p>Username: {userData.user.username}</p>
-			<p>Name: {userData.user.name}</p>
-		</figure>
-	);
-};
+class User extends Component {
+	state = {
+		userData: null
+	};
+
+	componentDidMount() {
+		api.fetchUsersData(this.props.username).then((data) => {
+			this.setState({ userData: data });
+		});
+	}
+
+	render() {
+		const { userData } = this.state;
+		return !userData ? (
+			<p>loading...</p>
+		) : (
+			<figure id="userProfile">
+				<img src={userData.user.avatar_url} alt="users avatar" />
+				<p>Username: {userData.user.username}</p>
+				<p>Name: {userData.user.name}</p>
+			</figure>
+		);
+	}
+}
 
 export default User;
