04c5beaea249b77f0ec6efc9dd1d6d1ece9f748e
Andrew <manloengchung@yahoo.co.uk>
2019-08-06 20:27:08 +0100

    fetch data when link changes

--- src/components/ArticleListByTopicSlug/ArticleListByTopicSlug.jsx
+++ src/components/ArticleListByTopicSlug/ArticleListByTopicSlug.jsx
@@ -8,22 +8,32 @@ class ArticleListByTopicSlug extends Component {
 	};
 
 	componentDidMount() {
+		this.fetchArticleDataByTopicSlug();
+	}
+
+	componentDidUpdate(prevProp) {
+		if (prevProp.topic_slug !== this.props.topic_slug) {
+			this.fetchArticleDataByTopicSlug();
+		}
+	}
+
+	fetchArticleDataByTopicSlug = () => {
 		const { topic_slug } = this.props;
-		api.fetchArticleDataByTopicSlug(topic_slug).then((articles) => {
+		api.getArticleDataByTopicSlug(topic_slug).then((articles) => {
 			this.setState({ articleDataByTopicSlug: articles });
 		});
-	}
+	};
 
 	render() {
 		const { articleDataByTopicSlug } = this.state;
-		return (
-			articleDataByTopicSlug && (
-				<section>
-					{articleDataByTopicSlug.map((article) => {
-						return <ArticleCard article={article} key={article.article_id} />;
-					})}
-				</section>
-			)
+		return !articleDataByTopicSlug ? (
+			<p>loading</p>
+		) : (
+			<section>
+				{articleDataByTopicSlug.map((article) => {
+					return <ArticleCard article={article} key={article.article_id} />;
+				})}
+			</section>
 		);
 	}
 }
