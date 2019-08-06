e7b36cbee04ed97ff825a1d47d2ecfba4180d4e2
Andrew <manloengchung@yahoo.co.uk>
2019-08-06 09:32:06 +0100

    refactor topicList to a class based component

--- src/components/TopicList/TopicList.jsx
+++ src/components/TopicList/TopicList.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TopicList.css';
 import { Link } from '@reach/router';
+import TopicForm from './TopicForm';
+import * as api from '../api';
 
-const TopicList = ({ topicsData }) => {
-	return (
-		<section id="topicSection">
-			<figure>
-				{topicsData.topics.map((topic) => {
-					return (
-						<li key={topic.slug}>
-							<Link to={`/explore/${topic.slug}`}>
-								<h2>{topic.slug}</h2>
-								<img />
-								<figcaption>{topic.description}</figcaption>
-							</Link>
-						</li>
-					);
-				})}
-			</figure>
-		</section>
-	);
-};
+class TopicList extends Component {
+	state = {
+		topicSlug: null,
+		topicDescription: null,
+		display: false
+	};
 
+	componentDidUpdate(prevProp, prevState) {
+		if (prevState.topicSlug !== this.state.topicSlug) {
+			const { display, ...restOfState } = this.state;
+			api.postTopicsData(restOfState);
+		}
+	}
+
+	render() {
+		const { topicsData } = this.props;
+		return (
+			topicsData && (
+				<section id="topicSection">
+					<figure>
+						<TopicForm fetchInputs={this.fetchInputs} />
+						{topicsData.topics.map((topic) => {
+							return (
+								<li key={topic.slug}>
+									<Link to={`/explore/${topic.slug}`}>
+										<h2>{topic.slug}</h2>
+										<img />
+										<figcaption>{topic.description}</figcaption>
+									</Link>
+								</li>
+							);
+						})}
+					</figure>
+				</section>
+			)
+		);
+	}
+
+	fetchInputs = (inputs) => {
+		this.setState(inputs);
+	};
+}
 export default TopicList;
