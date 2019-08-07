b48c83528d20244564f13c449db9c5e386b4b948
Andrew <manloengchung@yahoo.co.uk>
2019-08-07 14:44:12 +0100

    add user header

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as api from '../api';
 import ErrorPage from '../ErrorPage';
 import './user.css';
+import Header from '../Header/Header';
 
 class User extends Component {
 	state = {
@@ -27,15 +28,20 @@ class User extends Component {
 
 	render() {
 		const { userData, err } = this.state;
+		const { username } = this.props;
+		
 		if (err) return <ErrorPage {...err} />;
 		return !userData ? (
 			<p>loading...</p>
 		) : (
+			<>
+			<Header route={username}/>
 			<figure id="userProfile">
 				<img src={userData.avatar_url} alt="users avatar" />
 				<p>Username: {userData.username}</p>
 				<p>Name: {userData.name}</p>
 			</figure>
+			</>
 		);
 	}
 }
