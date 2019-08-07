44ad0d8b8145ad49651fa28cdb8d95f8dace4acd
Andrew <manloengchung@yahoo.co.uk>
2019-08-07 12:22:12 +0100

    update comment list

--- src/components/Article/CommentListByArticleId.jsx
+++ src/components/Article/CommentListByArticleId.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './CommentListByArticleId.css';
 import Button from 'react-bootstrap/Button';
 import * as api from '../api';
+import CommentForm from './CommentForm';
 
 class CommentListByArticleId extends Component {
 	state = {
@@ -21,14 +22,23 @@ class CommentListByArticleId extends Component {
 
 	handleClick = (e) => {
 		const { article_id } = this.props;
-		api.deleteCommentByCommentId(e.target.name, article_id).then((comments) => {
+		const { name } = e.target;
+		api.deleteCommentByCommentId(name, article_id).then((comments) => {
 			this.setState({ commentListData: comments });
+		});
+	};
+
+	updateCommentList = (comment) => {
+		this.setState((currentState) => {
+			return {
+				commentListData: [ comment, ...currentState.commentListData ]
+			};
 		});
 	};
 
 	render() {
 		const { commentListData } = this.state;
-		const { user } = this.props;
+		const { user, article_id } = this.props;
 		return !commentListData ? (
 			<p>loading...</p>
 		) : (
@@ -38,6 +48,7 @@ class CommentListByArticleId extends Component {
 						<p>No comments available</p>
 					) : (
 						<ul>
+							<CommentForm updateCommentList={this.updateCommentList} user={user} article_id={article_id} />
 							{commentListData.map((comment) => {
 								return (
 									<li key={comment.comment_id} id="comment">
