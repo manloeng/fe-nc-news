fbb076586e98565ccd3afa8e80b53f5e8a9666ff
Andrew <manloengchung@yahoo.co.uk>
2019-08-07 21:21:10 +0100

    apply working vote func

--- src/components/Voter/Voter.jsx
+++ src/components/Voter/Voter.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import * as api from '../api';
 
 class Voter extends Component {
+	state = {
+		inc_votes: 0
+	};
+
+	voteChange = (vote) => {
+		this.setState(({ inc_votes }) => {
+			return { inc_votes: inc_votes + vote };
+		});
+	};
+
+	componentDidUpdate(prevProps, prevState) {
+		const { inc_votes } = this.state;
+		const { article_id } = this.props;
+		if (prevState.inc_votes !== inc_votes) {
+			api.patchArticleVoteByArticleId(inc_votes, article_id);
+		}
+	}
+
 	render() {
 		const { votes } = this.props;
+		const { inc_votes } = this.state;
+
 		return (
 			<div>
-				<i className="fa fa-arrow-up" aria-hidden="true" />
-				<p>Votes: {votes}</p>
-				<i className="fa fa-arrow-down" aria-hidden="true" />
+				<button
+					onClick={(e) => {
+						this.voteChange(1);
+					}}
+					disabled={inc_votes >= 1}
+				>
+					<i className="fa fa-arrow-up" aria-hidden="true" />
+				</button>
+				<p>Votes: {inc_votes + votes}</p>
+				<button
+					onClick={(e) => {
+						this.voteChange(-1);
+					}}
+					disabled={inc_votes <= -1}
+				>
+					<i className="fa fa-arrow-down" aria-hidden="true" />
+				</button>
 			</div>
 		);
 	}
