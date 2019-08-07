e6519da4af7ce8a663fc90a1e6277788f1ed7dab
Andrew <manloengchung@yahoo.co.uk>
2019-08-07 17:24:28 +0100

    add sorter component

---
+++ src/components/ArticleSorter/ArticleSorter.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
+const ArticleSorter = ({ handleChange, sort_by, order }) => {
+	return (
+		<Form>
+			<Form.Row>
+				<Form.Group as={Col} controlId="sort_by">
+					<Form.Label>Sort By:</Form.Label>
+					<Form.Control as="select" onChange={handleChange}>
+						<option>Date</option>
+						<option>Author</option>
+						<option>Topic</option>
+					</Form.Control>
+				</Form.Group>
+
+				<Form.Group as={Col} controlId="order">
+					<Form.Label>Order By:</Form.Label>
+					<Form.Control as="select" onChange={handleChange}>
+						<option>Desc</option>
+						<option>Asc</option>
+					</Form.Control>
+				</Form.Group>
+			</Form.Row>
+		</Form>
+	);
+};
+
+export default ArticleSorter;
