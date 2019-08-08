d5059d6015c1d782d7b882ed429374c1475e07d8
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 09:46:52 +0100

    remove componentDidUpdate and update via onClick

--- src/components/Voter/Voter.jsx
+++ src/components/Voter/Voter.jsx
@@ -7,18 +7,13 @@ class Voter extends Component {
 	};
 
 	voteChange = (vote) => {
+		const { inc_votes } = this.state;
+		const { article_id } = this.props;
+		api.patchArticleVoteByArticleId(inc_votes, article_id);
 		this.setState(({ inc_votes }) => {
 			return { inc_votes: inc_votes + vote };
 		});
 	};
-
-	componentDidUpdate(prevProps, prevState) {
-		const { inc_votes } = this.state;
-		const { article_id } = this.props;
-		if (prevState.inc_votes !== inc_votes) {
-			api.patchArticleVoteByArticleId(inc_votes, article_id);
-		}
-	}
 
 	render() {
 		const { votes } = this.props;
