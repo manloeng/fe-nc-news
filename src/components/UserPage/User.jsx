115a6f265cd9bcb5c8025a561cc6c600c8f75125
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 10:56:58 +0100

    section user data

--- src/components/UserPage/User.jsx
+++ src/components/UserPage/User.jsx
@@ -3,7 +3,7 @@ import * as api from '../api';
 import ErrorPage from '../ErrorPage';
 import './user.css';
 import Header from '../Header/Header';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
 
 class User extends Component {
 	state = {
@@ -30,19 +30,19 @@ class User extends Component {
 	render() {
 		const { userData, err } = this.state;
 		const { username } = this.props;
-		
+
 		if (err) return <ErrorPage {...err} />;
 		return !userData ? (
 			<p>loading...</p>
 		) : (
-			<>
-			<Header route={username}/>
-			<figure id="userProfile">
-				<Image src={userData.avatar_url} alt="users avatar" fluid />
-				<p>Username: {userData.username}</p>
-				<p>Name: {userData.name}</p>
-			</figure>
-			</>
+			<section className="userSection">
+				<Header route={username} />
+				<figure id="userProfile">
+					<Image src={userData.avatar_url} alt="users avatar" fluid />
+					<p>Username: {userData.username}</p>
+					<p>Name: {userData.name}</p>
+				</figure>
+			</section>
 		);
 	}
 }
