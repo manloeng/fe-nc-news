98e4a129162fa3ed1b6a939491c7fd2bfd25478f
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 16:09:52 +0100

    add header to Error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header/Header';
 
 const ErrorPage = ({ msg, status }) => {
 	return (
 		<div>
+			<Header route={'Error'} />
 			<p>
 				<em>Oops! You landed on an Error!</em>
 			</p>
