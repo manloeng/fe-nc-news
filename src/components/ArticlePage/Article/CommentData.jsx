24b783930d02898599a7b855837d6d0b120ce775
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 21:10:20 +0100

    put comment into jumbotron and style

--- src/components/ArticlePage/Article/CommentData.jsx
+++ src/components/ArticlePage/Article/CommentData.jsx
@@ -3,13 +3,17 @@ import Button from 'react-bootstrap/Button';
 import * as api from '../../api';
 import Voter from '../../Voter/Voter';
 import { Link } from '@reach/router';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import './CommentData.css';
 
 const CommentData = ({ comment_id, author, body, created_at, votes, user, handleClick }) => {
 	return (
 		<div>
 			<li id="comment">
 				<Link to={`/users/${author}`}>By: {author}</Link>
-				<p>{body}</p>
+				<Jumbotron id="commentJumbo" fluid>
+					<p>{body}</p>
+				</Jumbotron>
 				<Voter votes={votes} comment_id={comment_id} />
 				<p>
 					<span role="img" aria-label="created_at">
