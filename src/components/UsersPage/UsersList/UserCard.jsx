93ae517cf3b946f1e4573279c0492ad533777820
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 15:34:56 +0100

    remove unused css

--- src/components/UsersPage/UsersList/UserCard.jsx
+++ src/components/UsersPage/UsersList/UserCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import Image from 'react-bootstrap/Image';
 
 const UserCard = ({ avatar_url, username, name }) => {
 	return (
 		<ul id="userCard">
 			<li>
 				<figure id="userProfile">
-					{/* <img src={avatar_url} alt="users avatar" /> */}
+					<Image src={avatar_url} alt="users avatar" fluid />
+					<br />
 					<Link to={`/users/${username}`}>Username: {username}</Link>
 					<p>Name: {name}</p>
 				</figure>
