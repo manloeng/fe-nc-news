b22ca7a512c11c7152ed45694969e26c846ba8fa
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 10:51:54 +0100

    add link to username

--- src/components/ArticlePage/Article/CommentData.jsx
+++ src/components/ArticlePage/Article/CommentData.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import * as api from '../../api';
 import Voter from '../../Voter/Voter';
+import { Link } from '@reach/router';
 
 const CommentData = ({ comment_id, author, body, created_at, votes, user, handleClick }) => {
 	return (
 		<div>
 			<li id="comment">
-				<p>By: {author}</p>
-				<br />
+				<Link to={`/users/${author}`}>By: {author}</Link>
 				<p>{body}</p>
-				<br />
-				<p>Created at: {api.convertDateFormat(created_at)}</p>
-				<p>Votes: {votes}</p>
 				<Voter votes={votes} comment_id={comment_id} />
+				<p>Created at: {api.convertDateFormat(created_at)}</p>
 				{user === author && (
 					<Button variant="danger" onClick={handleClick} name={comment_id}>
 						Delete Comment
