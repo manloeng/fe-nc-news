b0ca7ca9ecbf49b899af5ad325b40cfcf7dbcf7c
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 16:22:49 +0100

    add some error images

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from './Header/Header';
+import Image from 'react-bootstrap/Image';
+import './ErrorPage.css';
 
 const ErrorPage = ({ msg, status }) => {
+	const errRef = {
+		400: 'https://previews.123rf.com/images/mariusz_prusaczyk/mariusz_prusaczyk1205/mariusz_prusaczyk120500298/13700783-3d-error-400-crossword-on-white-background.jpg',
+		404: 'https://image.freepik.com/free-vector/funny-error-404-background-design_1167-219.jpg',
+		500: 'https://img.freepik.com/free-vector/500-error-cute-penguin-vector-illustration_81257-36.jpg?size=338&ext=jpg'
+	};
+
 	return (
 		<div>
 			<Header route={'Error'} />
+			{status && <Image src={errRef[status]} fluid id="errImg" />}
 			<p>
 				<em>Oops! You landed on an Error!</em>
 			</p>
