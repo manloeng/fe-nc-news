7dbd55ff3f193fb7737c082133b896b971b6a721
Andrew <manloengchung@yahoo.co.uk>
2019-08-08 11:55:07 +0100

    add pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import './Pagination.css';
+import * as api from '../api';
 
 class Pagination extends Component {
+	state = {
+		maxPage: Infinity,
+		currentPage: 1
+	};
+
+	componentDidMount() {
+		const { total_count, articles } = this.props.articleListData;
+		const maxPage = Math.ceil(total_count / articles.length);
+		this.setState({ maxPage });
+	}
+
+	updatePageCount = (pageCount) => {
+		this.setState(({ maxPage, currentPage }) => {
+			return { maxPage, currentPage: currentPage + pageCount };
+		});
+	};
+
+	componentDidUpdate(prevProps, prevState) {
+		const { currentPage } = this.state;
+		const { updateViaPagination } = this.props;
+		if (prevState.currentPage !== currentPage) {
+			api.getArticleData({ p: currentPage }).then((data) => {
+				updateViaPagination(data);
+			});
+		}
+	}
+
 	render() {
+		const { maxPage, currentPage } = this.state;
 		return (
-			<div>
-				<Button id="pagination">prev</Button>
-				<Button id="pagination">next</Button>
+			<div className="pagination">
+				<Button
+					onClick={() => {
+						this.updatePageCount(-1);
+					}}
+					disabled={currentPage === 1}
+				>
+					prev
+				</Button>
+				<p>
+					{currentPage} of {maxPage}
+				</p>
+				<Button
+					onClick={() => {
+						this.updatePageCount(1);
+					}}
+					disabled={currentPage === maxPage}
+				>
+					next
+				</Button>
 			</div>
 		);
 	}
