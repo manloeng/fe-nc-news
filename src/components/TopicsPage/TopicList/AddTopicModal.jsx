0276f99c8b503eda13af40f824328a941b96f7df
Andrew <manloengchung@yahoo.co.uk>
2019-08-09 11:22:31 +0100

    Create Topic modal component

---
+++ src/components/TopicsPage/TopicList/AddTopicModal.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import TopicForm from './TopicForm';
+
+class AddTopicModal extends Component {
+	state = {
+		show: false
+	};
+
+	handleShow = () => {
+		this.setState({ show: true });
+	};
+
+	handleClose = (e) => {
+		this.setState({ show: false });
+	};
+
+	render() {
+		const { show } = this.state;
+		const { user, updateTopicsList } = this.props;
+		return (
+			<div>
+				<Button variant="primary" onClick={this.handleShow}>
+					Add Topic
+				</Button>
+
+				<Modal show={show} onHide={this.handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Add Topic</Modal.Title>
+					</Modal.Header>
+					{user ? <TopicForm updateTopicsList={updateTopicsList} /> : <p>Please log in!</p>}
+				</Modal>
+			</div>
+		);
+	}
+}
+
+export default AddTopicModal;
