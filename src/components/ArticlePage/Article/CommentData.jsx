4bb2752eae8f19941e4fa5c12ed409b3ad7cc49f
Andrew <manloengchung@yahoo.co.uk>
2019-08-09 12:59:58 +0100

    delete un used code

--- src/components/ArticlePage/Article/CommentData.jsx
+++ src/components/ArticlePage/Article/CommentData.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import * as api from '../../api';
 import Voter from '../../Voter/Voter';
 import { Link } from '@reach/router';
