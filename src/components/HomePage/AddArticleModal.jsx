9a7f27496fdf8760d0318135af9ab3d1bb3843ab
Andrew <manloengchung@yahoo.co.uk>
2019-08-09 11:17:40 +0100

    import blank css file, just incase

--- src/components/HomePage/AddArticleModal.jsx
+++ src/components/HomePage/AddArticleModal.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import ArticleForm from '../ArticleForm/ArticleForm';
+import './AddArticleModal.css';
 
 class AddArticleModal extends Component {
 	state = {
