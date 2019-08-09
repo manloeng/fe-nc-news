1dcc6c8bbf08115fbf2a63548a4bdc489ea2cf03
Andrew <manloengchung@yahoo.co.uk>
2019-08-09 09:44:56 +0100

    remove unneeded html tags

--- src/components/UsersPage/UsersList/UserCard.jsx
+++ src/components/UsersPage/UsersList/UserCard.jsx
@@ -4,16 +4,12 @@ import Image from 'react-bootstrap/Image';
 
 const UserCard = ({ avatar_url, username, name }) => {
 	return (
-		<ul id="userCard">
-			<li>
-				<figure id="userProfile">
-					<Image src={avatar_url} alt="users avatar" fluid />
-					<br />
-					<Link to={`/users/${username}`}>Username: {username}</Link>
-					<p>Name: {name}</p>
-				</figure>
-			</li>
-		</ul>
+		<figure id="userProfile">
+			<Image src={avatar_url} alt="users avatar" fluid />
+			<br />
+			<Link to={`/users/${username}`}>Username: {username}</Link>
+			<p>Name: {name}</p>
+		</figure>
 	);
 };
 
