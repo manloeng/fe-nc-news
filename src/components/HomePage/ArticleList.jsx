c99e8855106135be11813a39c9ff6d21baaef7f8
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 12:11:26 +0100

    Remove unsed Code

--- src/components/HomePage/ArticleList.jsx
+++ src/components/HomePage/ArticleList.jsx
@@ -10,84 +10,77 @@ import Loader from '../Loader/Loader';
 import AddArticleModal from './AddArticleModal';
 
 class ArticleList extends Component {
-	state = {
-		articleListData: null,
-		err: null,
-		sort_by: 'Date',
-		order: 'Desc'
-	};
+  state = {
+    articleListData: null,
+    err: null,
+    sort_by: 'Date',
+    order: 'Desc'
+  };
 
-	componentDidMount() {
-		this.fetchArticleData();
-	}
+  componentDidMount() {
+    this.fetchArticleData();
+  }
 
-	fetchArticleData = (query) => {
-		api
-			.getArticleData(query)
-			.then((data) => {
-				this.setState({ articleListData: data });
-			})
-			.catch((err) => {
-				const { status, data } = err.response;
-				this.setState({ err: { status, msg: data.msg } });
-			});
-	};
+  fetchArticleData = (query) => {
+    api.getArticleData(query).then((data) => {
+      this.setState({ articleListData: data });
+    });
+  };
 
-	updateArticlesList = (article) => {
-		this.setState((currentState) => {
-			return {
-				articleListData: {
-					total_count: currentState.total_count,
-					articles: [ article, ...currentState.articleListData.articles ]
-				}
-			};
-		});
-	};
+  updateArticlesList = (article) => {
+    this.setState((currentState) => {
+      return {
+        articleListData: {
+          total_count: currentState.total_count,
+          articles: [ article, ...currentState.articleListData.articles ]
+        }
+      };
+    });
+  };
 
-	handleChange = (e) => {
-		const { id, value } = e.target;
-		this.setState({ [id]: value });
-	};
+  handleChange = (e) => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
+  };
 
-	updateViaPagination = (articleListData) => {
-		this.setState({ articleListData });
-	};
+  updateViaPagination = (articleListData) => {
+    this.setState({ articleListData });
+  };
 
-	componentDidUpdate(prevProp, prevState) {
-		let { sort_by, order } = this.state;
+  componentDidUpdate(prevProp, prevState) {
+    let { sort_by, order } = this.state;
 
-		if (prevState.sort_by !== sort_by || prevState.order !== order) {
-			if (sort_by === 'Date') {
-				sort_by = 'created_at';
-			}
-			if (sort_by === 'Comment Count') {
-				sort_by = 'comment_count';
-			}
-			this.fetchArticleData({ sort_by: sort_by.toLowerCase(), order: order.toLowerCase() });
-		}
-	}
+    if (prevState.sort_by !== sort_by || prevState.order !== order) {
+      if (sort_by === 'Date') {
+        sort_by = 'created_at';
+      }
+      if (sort_by === 'Comment Count') {
+        sort_by = 'comment_count';
+      }
+      this.fetchArticleData({ sort_by: sort_by.toLowerCase(), order: order.toLowerCase() });
+    }
+  }
 
-	render() {
-		const { articleListData, err } = this.state;
-		const { user, topicsData, path } = this.props;
+  render() {
+    const { articleListData } = this.state;
+    const { user, topicsData, path } = this.props;
 
-		if (err) return <ErrorPage {...err} />;
-		return !articleListData ? (
-			<Loader />
-		) : (
-			<div>
-				<section id="articleCardSection">
-					<Header route={path} />
-					<ArticleSorter handleChange={this.handleChange} />
-					<AddArticleModal user={user} topicsData={topicsData} updateArticlesList={this.updateArticlesList} />
-					{articleListData.articles.map((article) => {
-						return <ArticleCard {...article} key={article.article_id} />;
-					})}
-				</section>
-				<Pagination articleListData={articleListData} updateViaPagination={this.updateViaPagination} />
-			</div>
-		);
-	}
+    return !articleListData ? (
+      <Loader />
+    ) : (
+      <div>
+        <section id="articleCardSection">
+          <Header route={path} />
+          <ArticleSorter handleChange={this.handleChange} />
+          <AddArticleModal user={user} topicsData={topicsData} updateArticlesList={this.updateArticlesList} />
+          {articleListData.articles.map((article) => {
+            return <ArticleCard {...article} key={article.article_id} />;
+          })}
+        </section>
+        <Pagination articleListData={articleListData} updateViaPagination={this.updateViaPagination} />
+      </div>
+    );
+  }
 }
 
 export default ArticleList;
