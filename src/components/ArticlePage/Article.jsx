79991edca5cd620b2d8d611983abd122e7ba8c89
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 12:09:49 +0100

    Pass error to errorPage

--- src/components/ArticlePage/Article.jsx
+++ src/components/ArticlePage/Article.jsx
@@ -8,49 +8,49 @@ import Loader from '../Loader/Loader';
 import DeleteModal from '../DeleteModal/DeleteModal';
 
 class Article extends Component {
-	state = {
-		articleData: null,
-		err: null
-	};
+  state = {
+    articleData: null,
+    err: null
+  };
 
-	componentDidMount() {
-		this.fetchArticleDataByArticleId();
-	}
+  componentDidMount() {
+    this.fetchArticleDataByArticleId();
+  }
 
-	fetchArticleDataByArticleId = () => {
-		api
-			.getArticleDataByArticleId(this.props.article_id)
-			.then((article) => {
-				this.setState({ articleData: article });
-			})
-			.catch((err) => {
-				const { status, data } = err.response;
-				this.setState({ err: { status, msg: data.msg } });
-			});
-	};
+  fetchArticleDataByArticleId = () => {
+    api
+      .getArticleDataByArticleId(this.props.article_id)
+      .then((article) => {
+        this.setState({ articleData: article });
+      })
+      .catch((err) => {
+        const { status, data } = err.response;
+        this.setState({ err: { status, msg: data.msg } });
+      });
+  };
 
-	handleClick = (e) => {
-		const { article_id } = this.props;
-		api.deleteArticleByArticleId(article_id);
-	};
+  handleClick = (e) => {
+    const { article_id } = this.props;
+    api.deleteArticleByArticleId(article_id);
+  };
 
-	render() {
-		const { articleData, err } = this.state;
-		const { user, article_id } = this.props;
+  render() {
+    const { articleData, err } = this.state;
+    const { user, article_id } = this.props;
 
-		if (err) return <ErrorPage />;
-		return !articleData ? (
-			<Loader />
-		) : (
-			<section className="article">
-				<div className="articleDivider">
-					<ArticleData {...articleData} user={user} />
-					{user === articleData.author && <DeleteModal handleClick={this.handleClick} />}
-				</div>
-				<CommentListByArticleId article_id={article_id} user={user} />
-			</section>
-		);
-	}
+    if (err) return <ErrorPage {...err} />;
+    return !articleData ? (
+      <Loader />
+    ) : (
+      <section className="article">
+        <div className="articleDivider">
+          <ArticleData {...articleData} user={user} />
+          {user === articleData.author && <DeleteModal handleClick={this.handleClick} />}
+        </div>
+        <CommentListByArticleId article_id={article_id} user={user} />
+      </section>
+    );
+  }
 }
 
 export default Article;
