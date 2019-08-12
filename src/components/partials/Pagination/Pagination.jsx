a4b956f5a11235857a0384b57abdc7a16026e9de
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 13:47:46 +0100

    restructure files

--- src/components/partials/Pagination/Pagination.jsx
+++ src/components/partials/Pagination/Pagination.jsx
@@ -4,59 +4,59 @@ import './Pagination.css';
 import * as api from '../../api';
 
 class Pagination extends Component {
-	state = {
-		maxPage: Infinity,
-		currentPage: 1
-	};
+  state = {
+    maxPage: Infinity,
+    currentPage: 1
+  };
 
-	componentDidMount() {
-		const { total_count, articles } = this.props.articleListData;
-		const maxPage = Math.ceil(total_count / articles.length);
-		this.setState({ maxPage });
-	}
+  componentDidMount() {
+    const { total_count, articles } = this.props.articleListData;
+    const maxPage = Math.ceil(total_count / articles.length);
+    this.setState({ maxPage });
+  }
 
-	updatePageCount = (pageCount) => {
-		this.setState(({ maxPage, currentPage }) => {
-			return { maxPage, currentPage: currentPage + pageCount };
-		});
-	};
+  updatePageCount = (pageCount) => {
+    this.setState(({ maxPage, currentPage }) => {
+      return { maxPage, currentPage: currentPage + pageCount };
+    });
+  };
 
-	componentDidUpdate(prevProps, prevState) {
-		const { currentPage } = this.state;
-		const { updateViaPagination } = this.props;
-		if (prevState.currentPage !== currentPage) {
-			api.getArticleData({ p: currentPage }).then((data) => {
-				updateViaPagination(data);
-			});
-		}
-	}
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage } = this.state;
+    const { updateViaPagination } = this.props;
+    if (prevState.currentPage !== currentPage) {
+      api.getArticleData({ p: currentPage }).then((data) => {
+        updateViaPagination(data);
+      });
+    }
+  }
 
-	render() {
-		const { maxPage, currentPage } = this.state;
-		return (
-			<div className="pagination">
-				<Button
-					onClick={() => {
-						this.updatePageCount(-1);
-					}}
-					disabled={currentPage === 1}
-				>
-					prev
-				</Button>
-				<p>
-					{currentPage} of {maxPage}
-				</p>
-				<Button
-					onClick={() => {
-						this.updatePageCount(1);
-					}}
-					disabled={currentPage === maxPage}
-				>
-					next
-				</Button>
-			</div>
-		);
-	}
+  render() {
+    const { maxPage, currentPage } = this.state;
+    return (
+      <div className="pagination">
+        <Button
+          onClick={() => {
+            this.updatePageCount(-1);
+          }}
+          disabled={currentPage === 1}
+        >
+          prev
+        </Button>
+        <p>
+          {currentPage} of {maxPage}
+        </p>
+        <Button
+          onClick={() => {
+            this.updatePageCount(1);
+          }}
+          disabled={currentPage === maxPage}
+        >
+          next
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default Pagination;
