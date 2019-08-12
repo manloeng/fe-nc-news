7d14f9d452946542eec0faf093900f36f530e1c5
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 15:21:10 +0100

    refactor state - commentList to comments

--- src/components/ArticlePage/CommentListByArticleId.jsx
+++ src/components/ArticlePage/CommentListByArticleId.jsx
@@ -6,59 +6,59 @@ import CommentData from './CommentData';
 import Loader from '../partials/Loader/Loader';
 
 class CommentListByArticleId extends Component {
-	state = {
-		commentListData: null
-	};
+  state = {
+    comments: null
+  };
 
-	componentDidMount() {
-		this.fetchCommentDataByArticleId();
-	}
+  componentDidMount() {
+    this.fetchCommentDataByArticleId();
+  }
 
-	fetchCommentDataByArticleId = () => {
-		const { article_id } = this.props;
-		api.getCommentDataByArticleId(article_id).then((comments) => {
-			this.setState({ commentListData: comments });
-		});
-	};
+  fetchCommentDataByArticleId = () => {
+    const { article_id } = this.props;
+    api.getCommentDataByArticleId(article_id).then((comments) => {
+      this.setState({ comments });
+    });
+  };
 
-	handleClick = (e) => {
-		const { article_id } = this.props;
-		const { name } = e.target;
-		api.deleteCommentByCommentId(name, article_id).then((comments) => {
-			this.setState({ commentListData: comments });
-		});
-	};
+  handleClick = (e) => {
+    const { article_id } = this.props;
+    const { name } = e.target;
+    api.deleteCommentByCommentId(name, article_id).then((comments) => {
+      this.setState({ comments: comments });
+    });
+  };
 
-	updateCommentList = (comment) => {
-		this.setState((currentState) => {
-			return {
-				commentListData: [ comment, ...currentState.commentListData ]
-			};
-		});
-	};
+  updateCommentList = (comment) => {
+    this.setState((currentState) => {
+      return {
+        comments: [ comment, ...currentState.comments ]
+      };
+    });
+  };
 
-	render() {
-		const { commentListData } = this.state;
-		const { user, article_id } = this.props;
-		return !commentListData ? (
-			<Loader />
-		) : (
-			<section>
-				<article id="commentListByArticleId">
-					{user && <CommentForm updateCommentList={this.updateCommentList} user={user} article_id={article_id} />}
-					{!commentListData.length ? (
-						<p>No comments available</p>
-					) : (
-						<ul>
-							{commentListData.map((comment) => {
-								return <CommentData {...comment} user={user} key={comment.comment_id} handleClick={this.handleClick} />;
-							})}
-						</ul>
-					)}
-				</article>
-			</section>
-		);
-	}
+  render() {
+    const { comments } = this.state;
+    const { user, article_id } = this.props;
+    return !comments ? (
+      <Loader />
+    ) : (
+      <section>
+        <article id="commentListByArticleId">
+          {user && <CommentForm updateCommentList={this.updateCommentList} user={user} article_id={article_id} />}
+          {!comments.length ? (
+            <p>No comments available</p>
+          ) : (
+            <ul>
+              {comments.map((comment) => {
+                return <CommentData {...comment} user={user} key={comment.comment_id} handleClick={this.handleClick} />;
+              })}
+            </ul>
+          )}
+        </article>
+      </section>
+    );
+  }
 }
 
 export default CommentListByArticleId;
