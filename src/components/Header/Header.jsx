931140f13235fd1c6883050cfc81f2b713fb2202
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 12:14:21 +0100

    Remove comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import './Header.css';
 
 const Header = ({ route }) => {
-	const reg = /\w+/g;
-	if (route === '/') route = '/articles';
-	const matchedStr = route.match(reg)[0];
-	const formattedStr = matchedStr[0].toUpperCase() + matchedStr.slice(1);
-	return (
-		<header id="mainHeader">
-			<h1>{formattedStr}</h1>
-			{/* <Row className="addPost">
-		<button>
-			<i className="fa fa-plus-circle fa-3x" aria-hidden="true" />
-		</button>
-		</Row> */}
-		</header>
-	);
+  const reg = /\w+/g;
+  if (route === '/') route = '/articles';
+  const matchedStr = route.match(reg)[0];
+  const formattedStr = matchedStr[0].toUpperCase() + matchedStr.slice(1);
+  return (
+    <header id="mainHeader">
+      <h1>{formattedStr}</h1>
+    </header>
+  );
 };
 
 export default Header;
