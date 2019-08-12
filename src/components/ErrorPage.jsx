d65f0afa865d0865d6558d7ab06e96c3ec1c8b23
Andrew <manloengchung@yahoo.co.uk>
2019-08-12 12:12:24 +0100

    Add default to error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,36 +3,36 @@ import Header from './Header/Header';
 import Image from 'react-bootstrap/Image';
 import './ErrorPage.css';
 
-const ErrorPage = ({ msg, status }) => {
-	const errRef = {
-		400: 'https://previews.123rf.com/images/mariusz_prusaczyk/mariusz_prusaczyk1205/mariusz_prusaczyk120500298/13700783-3d-error-400-crossword-on-white-background.jpg',
-		404: 'https://image.freepik.com/free-vector/funny-error-404-background-design_1167-219.jpg',
-		500: 'https://img.freepik.com/free-vector/500-error-cute-penguin-vector-illustration_81257-36.jpg?size=338&ext=jpg',
-		undefined:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJthz4SmVOHGIi0YCVFz5p1cujZxye3VTGrBJverK3UArt9nGW'
-	};
+const ErrorPage = ({ msg = 'Page Not Found', status = '404' }) => {
+  const errRef = {
+    400: 'https://previews.123rf.com/images/mariusz_prusaczyk/mariusz_prusaczyk1205/mariusz_prusaczyk120500298/13700783-3d-error-400-crossword-on-white-background.jpg',
+    404: 'https://image.freepik.com/free-vector/funny-error-404-background-design_1167-219.jpg',
+    500: 'https://img.freepik.com/free-vector/500-error-cute-penguin-vector-illustration_81257-36.jpg?size=338&ext=jpg',
+    undefined:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJthz4SmVOHGIi0YCVFz5p1cujZxye3VTGrBJverK3UArt9nGW'
+  };
 
-	return (
-		<section>
-			<Header route={'Error'} />
-			<Image src={errRef[status]} fluid id="errImg" />
-			<div id="errorSection">
-				<p>
-					<em>Oops! You landed on an Error!</em>
-				</p>
-				{msg && (
-					<p>
-						<em>Status: {status}</em>
-					</p>
-				)}
-				{status && (
-					<p>
-						<em>{msg}...</em>
-					</p>
-				)}
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <Header route={'Error'} />
+      <Image src={errRef[status]} fluid id="errImg" />
+      <div id="errorSection">
+        <p>
+          <em>Oops! You landed on an Error!</em>
+        </p>
+        {msg && (
+          <p>
+            <em>Status: {status}</em>
+          </p>
+        )}
+        {status && (
+          <p>
+            <em>{msg}...</em>
+          </p>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
