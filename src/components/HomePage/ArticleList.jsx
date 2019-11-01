3c8f40400e3eb5dc3ff44d601ad52332da6f9560
Andrew Chung <andrewchung@fox-ache.local>
2019-11-01 15:16:34 +0000

    Refactor File with Hooks

--- src/components/HomePage/ArticleList.jsx
+++ src/components/HomePage/ArticleList.jsx
@@ -1,133 +1,128 @@
-import React, { Component } from 'react';
-import ArticleCard from '../HomePage/ArticleCard';
-import * as api from '../api';
-import Header from '../partials/Header/Header';
-import './ArticleList.css';
-import Pagination from '../partials/Pagination/Pagination';
-import Loader from '../partials/Loader/Loader';
-import ArticleSorter from '../partials/ArticleSorter/ArticleSorter';
-import AddArticleModal from './AddArticleModal';
-import ErrorPage from '../ErrorPage';
+import React, { useState, useEffect } from "react";
+import ArticleCard from "../HomePage/ArticleCard";
+import * as api from "../api";
+import Header from "../partials/Header/Header";
+import "./ArticleList.css";
+import Pagination from "../partials/Pagination/Pagination";
+import Loader from "../partials/Loader/Loader";
+import ArticleSorter from "../partials/ArticleSorter/ArticleSorter";
+import AddArticleModal from "./AddArticleModal";
+import ErrorPage from "../ErrorPage";
 
-class ArticleList extends Component {
-  state = {
-    articleListData: null,
-    topicErr: null,
-    err: null,
-    sort_by: 'Date',
-    order: 'Desc',
-    isLoading: true,
-    displayErr: false
-  };
+function ArticleList({ topic_slug, user, topics, path }) {
+  const [articleListData, setArticleListData] = useState("");
+  const [topicErr, setTopicErr] = useState({});
+  const [err, setErr] = useState("");
+  const [sort_by, setSortby] = useState("Date");
+  const [order, setOrder] = useState("Desc");
+  const [isLoading, setIsLoading] = useState(true);
+  const [displayErr, setDesplayErr] = useState(false);
 
-  componentDidMount() {
-    this.fetchArticleData();
-  }
+  useEffect(() => {
+    fetchArticleData();
+  }, []);
 
-  componentDidUpdate(prevProp, prevState) {
-    const { topic_slug } = this.props;
-
-    if (prevProp.topic_slug !== topic_slug) {
-      this.fetchArticleData();
+  useEffect(() => {
+    fetchArticleData();
+    if (sort_by === "Date") {
+      setSortby("created_at");
     }
+    if (sort_by === "Comment Count") {
+      setSortby("comment_count");
 
-    let { sort_by, order } = this.state;
-    if (prevState.sort_by !== sort_by || prevState.order !== order) {
-      if (sort_by === 'Date') {
-        sort_by = 'created_at';
-      }
-      if (sort_by === 'Comment Count') {
-        sort_by = 'comment_count';
-      }
-      this.fetchArticleData(sort_by.toLowerCase(), order.toLowerCase());
+      console.log(sort_by);
+      fetchArticleData(sort_by.toLowerCase(), order.toLowerCase());
     }
-  }
+  }, [topic_slug, sort_by, order]);
 
-  fetchArticleData = (sort_by, order) => {
-    const { topic_slug, topics } = this.props;
+  const fetchArticleData = (sort_by, order) => {
     const query = { topic: topic_slug, sort_by, order };
+    setIsLoading(true);
 
-    this.setState({ isLoading: true });
     api
       .getArticleData(query)
-      .then((data) => {
-        this.setState({ articleListData: data, err: null, displayErr: false, isLoading: false });
+      .then(data => {
+        setArticleListData(data);
+        setErr("");
+        setDesplayErr(false);
+        setIsLoading(false);
       })
-      .catch((err) => {
+      .catch(err => {
         const { status, data } = err.response;
-        topics.forEach((topic) => {
+        topics.forEach(topic => {
           if (topic_slug.trim() === topic.slug.trim()) {
-            this.setState({ err: { status, msg: data.msg }, topicErr: null, displayErr: true, isLoading: false });
+            setErr({ status, msg: data.msg });
+            setTopicErr("");
+            setDesplayErr(true);
+            setIsLoading(false);
           }
         });
-        if (!this.state.err) {
-          this.setState({
-            topicErr: { status, msg: 'Topic not found!' },
-            err: null,
-            displayErr: true,
-            isLoading: true
-          });
+        if (!err) {
+          setErr("Topic not found!");
+          setTopicErr("");
+          setDesplayErr(true);
+          setIsLoading(true);
         }
       });
   };
 
-  updateViaPagination = (articleListData) => {
-    this.setState({ articleListData });
+  const updateViaPagination = articleListData => {
+    setArticleListData(articleListData);
   };
 
-  handleQueryChange = ({ sort_by, order }) => {
-    this.setState({ sort_by, order });
+  const handleQueryChange = ({ sort_by, order }) => {
+    setSortby(sort_by);
+    setOrder(order);
   };
 
-  updateArticlesList = (article) => {
-    this.setState((currentState) => {
-      return {
-        articleListData: {
-          total_count: currentState.total_count,
-          articles: [ article, ...currentState.articleListData.articles ]
-        }
-      };
+  const updateArticlesList = article => {
+    console.log(articleListData);
+    setArticleListData({
+      total_count: articleListData.total_count,
+      articles: [article, ...articleListData.articles]
     });
   };
 
-  render() {
-    const { articleListData, topicErr, isLoading, displayErr } = this.state;
-    const { topic_slug, user, topics, path } = this.props;
+  if (displayErr && topicErr) return <ErrorPage {...topicErr} />;
+  if (isLoading) return <Loader />;
+  return (
+    <div id="topicsSection">
+      <section>
+        {/*  if theres no err show  header */}
+        {!topicErr && <Header route={topic_slug} />}
+        {displayErr && <p>No Articles Found</p>}
 
-    if (displayErr && topicErr) return <ErrorPage {...topicErr} />;
-    if (isLoading) return <Loader />;
-    return (
-      <div id="topicsSection">
-        <section>
-          {/*  if theres no err show  header */}
-          {!topicErr && <Header route={topic_slug} />}
-          {displayErr && <p>No Articles Found</p>}
-
-          {/*  if !err and articles are found show sorter and if path is '/' show modal*/}
-          {articleListData &&
-          !displayErr && <ArticleSorter handleQueryChange={this.handleQueryChange} /> &&
-          path === '/' && <AddArticleModal user={user} topics={topics} updateArticlesList={this.updateArticlesList} />}
-
-          {/*  prevent null error when refreshing on the topic page*/
-          articleListData &&
-            !displayErr &&
-            articleListData.articles.map((article) => {
-              return <ArticleCard {...article} key={article.article_id} />;
-            })}
-        </section>
-
-        {/*  if !err and articles are found show pagination*/}
+        {/*  if !err and articles are found show sorter and if path is '/' show modal*/}
         {articleListData &&
-        !displayErr && (
-          <Pagination
-            articleListData={articleListData}
-            updateViaPagination={this.updateViaPagination}
-            topic_slug={topic_slug}
-          />
-        )}
-      </div>
-    );
-  }
+          !displayErr && (
+            <ArticleSorter handleQueryChange={handleQueryChange} />
+          ) &&
+          path === "/" && (
+            <AddArticleModal
+              user={user}
+              topics={topics}
+              updateArticlesList={updateArticlesList}
+            />
+          )}
+
+        {/*  prevent null error when refreshing on the topic page*/
+        articleListData &&
+          !displayErr &&
+          articleListData.articles.map(article => {
+            return <ArticleCard {...article} key={article.article_id} />;
+          })}
+      </section>
+
+      {/*  if !err and articles are found show pagination*/}
+      {articleListData && !displayErr && (
+        <Pagination
+          articleListData={articleListData}
+          updateViaPagination={updateViaPagination}
+          topic_slug={topic_slug}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ArticleList;
---
+++ src/components/HomePage/OldArticleList.jsx
@@ -0,0 +1,133 @@
+// import React, { Component } from 'react';
+// import ArticleCard from '../HomePage/ArticleCard';
+// import * as api from '../api';
+// import Header from '../partials/Header/Header';
+// import './ArticleList.css';
+// import Pagination from '../partials/Pagination/Pagination';
+// import Loader from '../partials/Loader/Loader';
+// import ArticleSorter from '../partials/ArticleSorter/ArticleSorter';
+// import AddArticleModal from './AddArticleModal';
+// import ErrorPage from '../ErrorPage';
+
+// class ArticleList extends Component {
+//   state = {
+//     articleListData: null,
+//     topicErr: null,
+//     err: null,
+//     sort_by: 'Date',
+//     order: 'Desc',
+//     isLoading: true,
+//     displayErr: false
+//   };
+
+//   componentDidMount() {
+//     this.fetchArticleData();
+//   }
+
+//   componentDidUpdate(prevProp, prevState) {
+//     const { topic_slug } = this.props;
+
+//     if (prevProp.topic_slug !== topic_slug) {
+//       this.fetchArticleData();
+//     }
+
+//     let { sort_by, order } = this.state;
+//     if (prevState.sort_by !== sort_by || prevState.order !== order) {
+//       if (sort_by === 'Date') {
+//         sort_by = 'created_at';
+//       }
+//       if (sort_by === 'Comment Count') {
+//         sort_by = 'comment_count';
+//       }
+//       this.fetchArticleData(sort_by.toLowerCase(), order.toLowerCase());
+//     }
+//   }
+
+//   fetchArticleData = (sort_by, order) => {
+//     const { topic_slug, topics } = this.props;
+//     const query = { topic: topic_slug, sort_by, order };
+
+//     this.setState({ isLoading: true });
+//     api
+//       .getArticleData(query)
+//       .then((data) => {
+//         this.setState({ articleListData: data, err: null, displayErr: false, isLoading: false });
+//       })
+//       .catch((err) => {
+//         const { status, data } = err.response;
+//         topics.forEach((topic) => {
+//           if (topic_slug.trim() === topic.slug.trim()) {
+//             this.setState({ err: { status, msg: data.msg }, topicErr: null, displayErr: true, isLoading: false });
+//           }
+//         });
+//         if (!this.state.err) {
+//           this.setState({
+//             topicErr: { status, msg: 'Topic not found!' },
+//             err: null,
+//             displayErr: true,
+//             isLoading: true
+//           });
+//         }
+//       });
+//   };
+
+//   updateViaPagination = (articleListData) => {
+//     this.setState({ articleListData });
+//   };
+
+//   handleQueryChange = ({ sort_by, order }) => {
+//     this.setState({ sort_by, order });
+//   };
+
+//   updateArticlesList = (article) => {
+//     this.setState((currentState) => {
+//       return {
+//         articleListData: {
+//           total_count: currentState.total_count,
+//           articles: [ article, ...currentState.articleListData.articles ]
+//         }
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { articleListData, topicErr, isLoading, displayErr } = this.state;
+//     const { topic_slug, user, topics, path } = this.props;
+
+//     if (displayErr && topicErr) return <ErrorPage {...topicErr} />;
+//     if (isLoading) return <Loader />;
+//     return (
+//       <div id="topicsSection">
+//         <section>
+//           {/*  if theres no err show  header */}
+//           {!topicErr && <Header route={topic_slug} />}
+//           {displayErr && <p>No Articles Found</p>}
+
+//           {/*  if !err and articles are found show sorter and if path is '/' show modal*/}
+//           {articleListData &&
+//           !displayErr && <ArticleSorter handleQueryChange={this.handleQueryChange} /> &&
+//           path === '/' && <AddArticleModal user={user} topics={topics} updateArticlesList={this.updateArticlesList} />}
+
+//           {/*  prevent null error when refreshing on the topic page*/
+//           articleListData &&
+//             !displayErr &&
+//             articleListData.articles.map((article) => {
+//               return <ArticleCard {...article} key={article.article_id} />;
+//             })}
+//         </section>
+
+//         {/*  if !err and articles are found show pagination*/}
+//         {articleListData &&
+//         !displayErr && (
+//           <Pagination
+//             articleListData={articleListData}
+//             updateViaPagination={this.updateViaPagination}
+//             topic_slug={topic_slug}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default ArticleList;
