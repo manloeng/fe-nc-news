6926b20606161b7b74239469cf0fe70b8127220e
Andrew Chung <andrewchung@fox-ache.local>
2019-11-01 14:26:25 +0000

    Refactor File with Hooks

--- src/components/HomePage/AddArticleModal.jsx
+++ src/components/HomePage/AddArticleModal.jsx
@@ -1,48 +1,36 @@
-import React, { Component } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import ArticleForm from './ArticleForm/ArticleForm';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import ArticleForm from "./ArticleForm/ArticleForm";
 
-class AddArticleModal extends Component {
-  state = {
-    show: false
-  };
+function AddArticleModal({ user, topics, updateArticlesList }) {
+  const [show, setShow] = useState(false);
 
-  handleShow = () => {
-    this.setState({ show: true });
-  };
+  const toggle = () => setShow(!show);
 
-  handleClose = (e) => {
-    this.setState({ show: false });
-  };
+  return (
+    <div>
+      <Button variant="primary" onClick={toggle}>
+        Add Article
+      </Button>
 
-  render() {
-    const { show } = this.state;
-    const { user, topics, updateArticlesList } = this.props;
-    return (
-      <div>
-        <Button variant="primary" onClick={this.handleShow}>
-          Add Article
-        </Button>
-
-        <Modal show={show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Article</Modal.Title>
-          </Modal.Header>
-          {user ? (
-            <ArticleForm
-              user={user}
-              topics={topics}
-              updateArticlesList={updateArticlesList}
-              handleClose={this.handleClose}
-            />
-          ) : (
-            <p>Please log in!</p>
-          )}
-        </Modal>
-      </div>
-    );
-  }
+      <Modal show={show} onHide={toggle}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Article</Modal.Title>
+        </Modal.Header>
+        {user ? (
+          <ArticleForm
+            user={user}
+            topics={topics}
+            updateArticlesList={updateArticlesList}
+            handleClose={toggle}
+          />
+        ) : (
+          <p>Please log in!</p>
+        )}
+      </Modal>
+    </div>
+  );
 }
 
 export default AddArticleModal;
