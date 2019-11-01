cb2b22e2d44dbd6d106ddb849c89d66c723d498f
Andrew Chung <andrewchung@fox-ache.local>
2019-11-01 14:22:02 +0000

    Refactor File with Hooks

--- src/components/TopicsPage/OldAddTopicModal.jsx
+++ src/components/TopicsPage/OldAddTopicModal.jsx
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import TopicForm from './TopicForm';
+// import React, { Component } from 'react';
+// import Modal from 'react-bootstrap/Modal';
+// import Button from 'react-bootstrap/Button';
+// import TopicForm from './TopicForm';
 
-class AddTopicModal extends Component {
-	state = {
-		show: false
-	};
+// class AddTopicModal extends Component {
+// 	state = {
+// 		show: false
+// 	};
 
-	handleShow = () => {
-		this.setState({ show: true });
-	};
+// 	handleShow = () => {
+// 		this.setState({ show: true });
+// 	};
 
-	handleClose = (e) => {
-		this.setState({ show: false });
-	};
+// 	handleClose = (e) => {
+// 		this.setState({ show: false });
+// 	};
 
-	render() {
-		const { show } = this.state;
-		const { user, updateTopicsList } = this.props;
-		return (
-			<div>
-				<Button variant="primary" onClick={this.handleShow}>
-					Add Topic
-				</Button>
+// 	render() {
+// 		const { show } = this.state;
+// 		const { user, updateTopicsList } = this.props;
+// 		return (
+// 			<div>
+// 				<Button variant="primary" onClick={this.handleShow}>
+// 					Add Topic
+// 				</Button>
 
-				<Modal show={show} onHide={this.handleClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>Add Topic</Modal.Title>
-					</Modal.Header>
-					{user ? (
-						<TopicForm updateTopicsList={updateTopicsList} handleClose={this.handleClose} />
-					) : (
-						<p>Please log in!</p>
-					)}
-				</Modal>
-			</div>
-		);
-	}
-}
+// 				<Modal show={show} onHide={this.handleClose}>
+// 					<Modal.Header closeButton>
+// 						<Modal.Title>Add Topic</Modal.Title>
+// 					</Modal.Header>
+// 					{user ? (
+// 						<TopicForm updateTopicsList={updateTopicsList} handleClose={this.handleClose} />
+// 					) : (
+// 						<p>Please log in!</p>
+// 					)}
+// 				</Modal>
+// 			</div>
+// 		);
+// 	}
+// }
 
-export default AddTopicModal;
+// export default AddTopicModal;
---
+++ src/components/TopicsPage/OldTopicForm.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import * as api from '../api';
+
+class TopicForm extends Component {
+  state = {
+    slug: null,
+    description: null
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    const { handleClose } = this.props;
+    e.preventDefault();
+    handleClose();
+    const { updateTopicsList } = this.props;
+    const { ...restOfState } = this.state;
+    console.log(restOfState)
+    api
+      .postTopicData(restOfState)
+      .then(() => {
+        return api.getTopicsData();
+      })
+      .then((data) => {
+        updateTopicsList(data);
+      });
+  };
+
+  render() {
+    return (
+      <Form id="topicInput" onSubmit={this.handleSubmit}>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Topic Title: </Form.Label>
+          <Form.Control type="text" placeholder="Enter Topic Title" name="slug" onChange={this.handleChange} required />
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Topic Description: </Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            name="description"
+            placeholder="Enter Topic Description"
+            onChange={this.handleChange}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default TopicForm;
